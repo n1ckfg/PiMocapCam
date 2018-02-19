faa88df8bec18cdeaf087dc16c811d7883c42c8b
Nick Fox-Gieg <nick@fox-gieg.com>
2018-02-19 18:39:24 -0500

    change hostname

--- src/testApp.h
+++ src/testApp.h
@@ -5,7 +5,7 @@
 #include "ofxCvPiCam.h"
 //#include "TerminalListener.h"
 #include "ofxOsc.h"
-#define HOST "255.255.255.255"
+#define HOST "Allosaurus.local"
 #define PORT 7110
 
 class testApp : public ofBaseApp {
