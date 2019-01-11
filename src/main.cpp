052bfcdd736f3b7d1410672d449c33511287f932
Nick Fox-Gieg <nick@fox-gieg.com>
2019-01-11 15:23:13 -0500

    Run headless

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include "ofMain.h"
 #include "ofApp.h"
+#include "ofAppNoWindow.h"
 
 int main() {
-	ofSetupOpenGL(640, 480, OF_WINDOW);
+	ofAppNoWindow w;
+	ofSetupOpenGL(&w, 640, 480, OF_WINDOW);
 	ofRunApp(new ofApp());
 }
